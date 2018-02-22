26e40e37ec44858eb06f2659a50c1f366a91d58f
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-02-22 14:03:19 -0600

    Comment the child controller sections

--- server/public/scripts/controllers/child.controller.js
+++ server/public/scripts/controllers/child.controller.js
@@ -2,21 +2,23 @@ myApp.controller('ChildController', ['$http', 'FamilyService', 'ChildService', f
 	console.log('ChildController created');
 	var self = this;
 
+	// booleans to toggle content in the view
 	self.showAddChild = false;
 	self.addChildBtn = true;
 	self.removeChildButton = false;
 	self.showChildInfo = false;
 	self.showAddChild = ChildService.showAddChild;
 
+	// ChildService variables
 	self.childService = ChildService;
-	
 	self.categoryList = ChildService.categoryList;
+	self.message = ChildService.message;
 
+	// FamilyService variables
 	self.familyService = FamilyService;
 	self.familyObject = FamilyService.familyObject;
 
-	self.message = ChildService.message;
-
+	// Child object 
 	self.child = {
 		first_name: '',
 		dob: '',
@@ -24,24 +26,32 @@ myApp.controller('ChildController', ['$http', 'FamilyService', 'ChildService', f
 		family_id: self.familyObject.id
 	};
 
+	// ChildSerivce to get the list of children
 	self.getChildList = function (familyId) {
 		ChildService.getChildList(self.familyObject.id);
 	}
 
+	// Run the getChildList function
 	self.getChildList(self.familyObject.id);
 
+	// Get the childList variable
 	self.childList = ChildService.childList;
 
+	// ChildSerivce to add a child
 	self.addChild = function (child, familyId) {
 		ChildService.addChild(child, self.familyObject.id);
 	}
 
+	// ChildSerivce to remove a child
 	self.deleteChild = function (id, familyId) {
 		ChildService.deleteChild(id, self.familyObject.id);
 	}
 
+	// ChildSerivce to get the category list
 	self.getCategoryList = function () {
 		ChildService.getCategoryList();
 	}
+
+	// Run the getCategoryList function
 	self.getCategoryList();
 }]);
