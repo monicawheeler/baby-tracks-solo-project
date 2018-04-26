73a2b571b7414c1990429b07033907616a8673c3
Monica Wheeler <mowheeler@netflixcontractors.com>
2018-04-26 17:06:10 -0500

    Get children and display message when no events to report

--- server/public/scripts/services/report.service.js
+++ server/public/scripts/services/report.service.js
@@ -2,4 +2,7 @@ myApp.service('ReportService', ['$http', '$location', function ($http, $location
     console.log('ReportService Loaded');
     var self = this;
 
+
+    //get request to get the children events 
+
 }]);
