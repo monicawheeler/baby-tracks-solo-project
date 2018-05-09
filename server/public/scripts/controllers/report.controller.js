11fb1ea50a351806e90f066568cee98b055cbbc5
Monica Wheeler <mowheeler@netflixcontractors.com>
2018-05-09 08:19:30 -0500

    Start routing process

--- server/public/scripts/controllers/report.controller.js
+++ server/public/scripts/controllers/report.controller.js
@@ -22,16 +22,10 @@ myApp.controller('ReportController', ['FamilyService', 'ReportService', 'EventSe
     // EventService to get the event list for the current child
     self.getChildEventList = function (id) {
         EventService.getChildEventList(id);
-        
     }
 
-    // TODO use onchange to grab the picked date from datepicker
-    // run a function that displays only events from current date
-    self.selectedDate = function(today) {
-        console.log('date', today)
+    self.sortBySelectedDate = function(theDay, id) {
+        theDay = theDay.toLocaleString();
+        ReportService.sortBySelectedDate(theDay, self.familyObject.id);
     }
-
-    // function that queries with a get request based on a 
-    // conditional greater than the picked date
-
 }]);
--- server/public/scripts/services/report.service.js
+++ server/public/scripts/services/report.service.js
@@ -2,7 +2,25 @@ myApp.service('ReportService', ['$http', '$location', function ($http, $location
     console.log('ReportService Loaded');
     var self = this;
 
+    // Storage for sorted events
+    self.sortedEvents = {
+        list: []
+    };
+
+    // TODO figure out how to send a date and in what format
+    // in order to store and use as a get request
 
     //get request to get the children events 
-
+    self.sortBySelectedDate = function(theDay, id) {
+        console.log('theDay', theDay);
+        
+        $http.get(`/api/report/family/${id}`)
+            .then(function (response) {
+                self.sortedEvents.list = response.data;
+            })
+            .catch(function (error) {
+                console.log('error, response:', response);
+                self.message = "Something went wrong. Please try again."
+            });
+        };
 }]);
